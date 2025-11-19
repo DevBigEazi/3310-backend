60728fb51a2a97851b7ae49c32c0fc94537251f9
DevBigEazi <adesholatajudeen1@gmail.com>
2025-11-19 14:25:07 +0100

    feat: add a function to get week number

---
+++ src/utils/helpers.ts
@@ -0,0 +1,9 @@
+// Get current ISO week number
+export function getWeekNumber(date = new Date()): number {
+  const d = new Date(Date.UTC(date.getFullYear(), date.getMonth(), date.getDate()));
+  const dayNum = d.getUTCDay() || 7;
+  d.setUTCDate(d.getUTCDate() + 4 - dayNum);
+  const yearStart = new Date(Date.UTC(d.getUTCFullYear(), 0, 1));
+  return Math.ceil((((d.getTime() - yearStart.getTime()) / 86400000) + 1) / 7);
+}
+
