5b76ff187aa42f88a1f72fb9615cfc3460a2c4a7
DevBigEazi <adesholatajudeen1@gmail.com>
2025-11-21 12:59:21 +0100

    refactor: add unique property to email and username

--- src/models/Player.ts
+++ src/models/Player.ts
@@ -7,15 +7,21 @@ export interface IPlayer extends Document {
   createdAt: Date;
   totalScoresSubmitted: number;
   lifetimeEarnings: number;
+  referredBy?: string;
+  referralCode: string;
+  referralPoints: number;
 }
 
 const playerSchema = new Schema<IPlayer>({
   address: { type: String, required: true, unique: true, lowercase: true },
-  username: String,
-  email: String,
+  username: { type: String, required: true, unique: true },
+  email: { type: String, unique: true, lowercase: true },
   createdAt: { type: Date, default: Date.now },
   totalScoresSubmitted: { type: Number, default: 0 },
-  lifetimeEarnings: { type: Number, default: 0 }
+  lifetimeEarnings: { type: Number, default: 0 },
+  referredBy: { type: String, lowercase: true },
+  referralCode: { type: String, unique: true },
+  referralPoints: { type: Number, default: 0 }
 });
 
 export const Player = mongoose.model<IPlayer>('Player', playerSchema);
